48a67ae554570f4f0679b61cc7ba5ff942d181a1
udayvaidya13 <udayvaidya13@gmail.com>
2024-11-07 00:55:27 +0530

    updated UI

--- server/server.js
+++ server/server.js
@@ -67,6 +67,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
